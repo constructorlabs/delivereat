234a5d940716fa7c9f77080a3d9bc2045834e6a3
dgrabov <d.grabov@gmail.com>
2018-02-22 14:49:31 +0000

    Add order functionality

--- server.js
+++ server.js
@@ -1,39 +1,58 @@
 const express = require('express');
 const hbs = require('hbs');
+const bodyParser = require('body-parser');
 const app = express();
 
 app.set('view engine', 'hbs');
 app.use('/static', express.static('static'));
+app.use(bodyParser.json());
+
+const storage = [];
 
 app.get('/', function(req, res){
   res.render('index', {});
+});
+
+app.get('/admin', function(req, res){
+  res.render('admin', {});
 });
 
 app.get('/api/menu', function(req, res){
   res.json({
     menu:[
       {
+        key:'five',
         name: 'Five Points IPA',
         price: 5.00
       },
       {
+        key:'beaver',
         name: 'Beavertown Smoked port',
         price: 5.50
       },
       {
+        key:'hell',
         name: 'Camden Hells',
         price: 4.70
       },
       {
+        key: 'dpony',
         name: 'Brewdog Dead Pony',
         price: 2.50
       },
       {
+        key: 'oldrosy',
         name: 'Old Rosy',
         price: 3.50
       }
     ]
   });
+});
+
+app.post('/api/orders', function(req, res){
+  storage.push(req.body);
+  res.status(404).end();
+  //.json(req.body);
 });
 
 app.listen(8080, function() { // Set app to listen for requests on port 3000
--- static/client.js
+++ static/client.js
@@ -6,8 +6,8 @@ function displayMenu(menu){
   menu.forEach(function(menuItem){
     const itemContainer = document.createElement('li');
     itemContainer.innerHTML = `
-      <span>${menuItem.name}</span>
-      <span>${menuItem.price}</span>
+      <div class="menu-item">${menuItem.name} <span>${menuItem.price}</span></div>
+      <input type="number" data-key="${menuItem.key}" class="quantity" />
     `;
     listContainer.appendChild(itemContainer);
   });
@@ -22,3 +22,41 @@ fetch('/api/menu')
   .then(function(menuWrapper){
     displayMenu(menuWrapper.menu);
   });
+
+const submitButton = document.getElementById('order-form');
+
+submitButton.addEventListener('submit', function( event ){
+  event.preventDefault();
+
+  const inputs = document.querySelectorAll('.quantity');
+
+  const order = {}
+
+  inputs.forEach(function(input){
+
+    const key = input.dataset.key;
+    const valueAsNumber = input.valueAsNumber;
+    if(valueAsNumber){
+      order[key] = valueAsNumber;
+    }
+  });
+
+  fetch('/api/orders', {
+    body: JSON.stringify(order),
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json'
+    }
+  })
+  .then(function(response){
+    if(response.status === 201){
+      alert('order accepted');
+    } else {
+      alert('something went horribly wrong!');
+    }
+  })
+});
+
+function responseHandler(response){
+
+}
