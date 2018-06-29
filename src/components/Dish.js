67ea884f695f3e2ea5d9dd78aed17d76c50f1f18
Ralph <ralph@rgalan.es>
2018-06-29 15:29:20 +0100

    Added add to cart button

--- src/components/Dish.js
+++ src/components/Dish.js
@@ -9,14 +9,13 @@ class Dish extends React.Component {
         const { image, name, ingredients, price } = this.props;
         return (
             <div className="dish">
+                <div className="dish__addToCart">
+                    <span className="dish__addToCart-icon">+</span>Cart
+                </div>
                 <div className="dish__image" style={{ backgroundImage: `url(${image})` }}></div>
-
-                {/* <div className="dish__image-wrapper">
-                    <img className="dish__image" src={image} alt="" />
-                </div> */}
                 <div className="dish__details">
                     <h2 className="dish__heading">{name}</h2>
-                    <p className="dish__ingredients">{ingredients} </p>
+                    <p className="dish__ingredients">{ingredients}.</p>
                     {/* <ul className="dish__ingredients">
                         {ingredients.map(ingredient => {
                             return <li className="dish__ingredients--ingredient" key={ingredient}>{ingredient}, </li>
