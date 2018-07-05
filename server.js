8bd1be3c6f0abe11550e136456c41e3ae2d5b1f1
Hamzah Kurdi <hamzahkurdi@aol.com>
2018-07-05 15:22:15 +0100

    heroku server activated

--- server.js
+++ server.js
@@ -163,6 +163,7 @@ app.get("*", function(req, res) {
   res.render("index");
 });
 
-app.listen(8080, function() {
-  console.log("Listening on port 8080");
+const port = process.env.PORT || 8080;
+app.listen(port, function() {
+  console.log(`Listening on port number ${port}`);
 });
