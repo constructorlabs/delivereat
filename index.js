40d5b0ad5c0a6fe1ae1de31338c057eac561d8ed
Ethan Ng <ethanng329@gmail.com>
2018-09-21 14:40:27 +0100

    fixing path

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
-app.use(bodyParser.json());
-app.use('/static', express.static(__dirname + '/static'));
 app.set('view engine', 'hbs');
+
+app.use(bodyParser.json());
+app.use('/static', express.static('static'));
 
 const menu = {
   1: {
