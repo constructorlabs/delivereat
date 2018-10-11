056408ef27c5cd108b279ad336efd02db2627bca
Joe Lamb <joe@elevenplustwo.co.uk>
2018-10-11 16:36:36 +0100

    hook up API to react view

--- server.js
+++ server.js
@@ -3,21 +3,34 @@ const bodyParser = require('body-parser');
 const app = express();
 
 app.use(bodyParser.json());
+
+// Loads bundle.js to load ReactJs components
 app.use('/static', express.static('static'));
 app.set('view engine', 'hbs');
 
 const menu = {
   1: {
     id: 1,
-    name: "Strawberry cheesecake",
-    price: 6
+    name: 'Cheeseburger',
+    price: 6,
+    img: ''
   }
 };
 
-app.get('/', function(req, res){
+// create a new object / array to store orders
+
+// handles first request from the user to localhost:8080 and call the index.hbs view to return an html page to the useri
+
+app.get('/', function(req, res) {
   res.render('index');
 });
 
-app.listen(8080, function(){
+app.get('/api/menu', function(req, res) {
+  res.json(menu);
+});
+
+// Server code is running in the server
+
+app.listen(8080, function() {
   console.log('Listening on port 8080');
 });
--- src/components/App.js
+++ src/components/App.js
@@ -3,16 +3,22 @@ import React from 'react';
 import '../styles/App.scss';
 
 class App extends React.Component {
-  constructor(){
+  constructor() {
     super();
   }
 
-  render(){
-    return (
-      <div>
-        Delivereat app
-      </div>
-    )
+  // React code is running in the browser
+
+  // create a fetch to a relative path eg. fetch("/api/menu") not "localhost:8080/api/menu" to the internal API from the endpoints defined in server.js  eg /Menu  /Order  etc
+
+  componentDidMount() {
+    fetch('/api/menu')
+      .then(response => response.json())
+      .then(body => console.log(body));
+  }
+
+  render() {
+    return <div>Delivereat app</div>;
   }
 }
 
