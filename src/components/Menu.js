c6793470b4fb2e7f1636b6bf7842bd058ffa2055
Joe Lamb <joe@elevenplustwo.co.uk>
2018-10-13 20:28:54 +0100

    menu by category

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -13,26 +13,58 @@ const Menu = ({ menu, handleMenuItemClick }) => {
 
   console.log(categories);
 
-  console.log(
-    menu.filter(item => {
-      return item.category == categories[0];
-    })
-  );
+  categories.forEach(menuCategory => {
+    console.log(
+      menu.filter(menuItem => {
+        return menuItem.category == menuCategory;
+      })
+    );
+  });
+
+  // console.log(
+  //   categories.forEach(category => {
+  //     return categories[i];
+  //   })
+  // );
+
+  // console.log(
+  //   menu.filter(item => {
+  //     return item.category == categories[0];
+  //   })
+  // );
 
   return (
     <div className="menu">
       <h2 className="menu__title">All-day Breakfast</h2>
-      {menu.map(item => {
-        return (
-          <MenuItem
-            key={item.id}
-            name={item.name}
-            price={item.price}
-            id={item.id}
-            handleMenuItemClick={handleMenuItemClick}
-          />
-        );
-      })};
+      <dl>
+        {categories.map(menuCategory => {
+          // console.log(menuCategory);
+          return (
+            <React.Fragment key={menuCategory}>
+              <dt>{menuCategory}</dt>
+              <dd>
+                <ul className="menu__list">
+                  {menu
+                    .filter(item => {
+                      return item.category == menuCategory;
+                    })
+                    .map(item => {
+                      return (
+                        <MenuItem
+                          key={item.id}
+                          name={item.name}
+                          price={item.price}
+                          id={item.id}
+                          handleMenuItemClick={handleMenuItemClick}
+                        />
+                      );
+                    })}
+                </ul>
+              </dd>
+            </React.Fragment>
+          );
+        })}
+      </dl>
     </div>
   );
 };
