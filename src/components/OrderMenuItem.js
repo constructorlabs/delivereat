e9c03213f729a1aff2d61eabbfacc6b2ea1348d4
David Gridley <davidgridley21@gmail.com>
2018-10-14 11:34:42 +0100

    added class switching

--- src/components/OrderMenuItem.js
+++ src/components/OrderMenuItem.js
@@ -1,12 +1,28 @@
 import React from "react";
 import Extras from "./Extras";
 
-function OrderMenuItem({ item, receiveOrder, extras, formatToMoney, currentOrder }) {
+function OrderMenuItem({
+  item,
+  receiveOrder,
+  extras,
+  formatToMoney,
+  currentOrder
+}) {
   return (
     <div className="order__menu__item">
       <h4 className="order__menu__item__name">{item.name}</h4>
       <p className="order__menu__item__price">{formatToMoney(item.price)}</p>
-      <button className="order__menu__item__add" onClick={() => receiveOrder(item)}>
+      <Extras
+        extras={extras}
+        itemId={item.id}
+        formatToMoney={formatToMoney}
+        currentOrder={currentOrder}
+        item={item}
+      />
+      <button
+        className="order__menu__item__add"
+        onClick={() => receiveOrder(item)}
+      >
         Add to order
       </button>
     </div>
