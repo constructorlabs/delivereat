bb30747f33183c29eecde01f4c7bd9cca82ba37c
David Gridley <davidgridley21@gmail.com>
2018-10-15 06:48:47 +0100

    added images

--- src/components/MenuPage.js
+++ src/components/MenuPage.js
@@ -6,6 +6,9 @@ function MenuPage({ menuClasses, menuObject, formatToMoney }) {
     <div className={menuClasses}>
       <h2 className="menu__heading">Our Menu</h2>
       <h3 className="menu__subheading">Burgers</h3>
+      <img
+        src="https://images.unsplash.com/photo-1520072959219-c595dc870360?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=80a0e355a5f83d806f9b72782c7fa19b&auto=format&fit=crop&w=853&q=80"
+      />
       {menuObject.burgers.map(item => (
         <MenuItem
           key={"menu" + item.name + item.id}
@@ -14,6 +17,9 @@ function MenuPage({ menuClasses, menuObject, formatToMoney }) {
         />
       ))}
       <h3 className="menu__subheading">Fries</h3>
+      <img
+        src="https://images.unsplash.com/photo-1534938665420-4193effeacc4?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=1ed7648548bd3112e6664027b01694b1&auto=format&fit=crop&w=751&q=80"
+      />
       {menuObject.fries.map(item => (
         <MenuItem
           key={"menu" + item.name + item.id}
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -17,9 +17,9 @@ class Navigation extends React.Component {
                 <ul className="nav__bar__list">
                     <li className="nav__bar__item__dummy">Home</li>
                     <li className="nav__bar__item__dummy">About</li>
-                    <li className="nav__bar__item__dummy">Locations</li>
                     <li className="nav__bar__item" id="menu" onClick={this.handleClick} >Menu</li>
                     <li className="nav__bar__item" id="order" onClick={this.handleClick} >Order</li>
+                    <li className="nav__bar__item__dummy">Locations</li>
                     <li className="nav__bar__item__dummy">Contact</li>
                 </ul>
             </nav>
